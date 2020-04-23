a423f7b3ecd581736f6654c73f0ca51e863f3fa9
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-04-23 18:07:51 +0300

    Add homework Unity

---
+++ Assignment 5/solutions/homework/homework/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+
+    [Tooltip("in meters per second")]
+    [SerializeField] float speed = 3;
+
+    [Header("Oscillation - Amplitude & Direction")]
+    public Vector3 oscillationVector;
+
+    private Vector3 oldPos;
+    public float deltaPhase = 2 * Mathf.PI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        oscillationVector = new Vector3(6, 0, 0);
+        uint i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uint i = 0;
+        float phase = speed * Time.time + i * deltaPhase ;
+        transform.position = oldPos + oscillationVector  * Mathf.Sin(phase);
+        i++;
+    }
+}
