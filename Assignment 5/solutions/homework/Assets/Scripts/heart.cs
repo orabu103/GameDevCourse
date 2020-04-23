d3d5dae932556abc2dcc80e00d79afa2d874cd21
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-04-23 20:45:42 +0300

    Heartbeat homework question 1.3

---
+++ Assignment 5/solutions/homework/Assets/Scripts/heart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Tooltip("in meters per second")]
+    [SerializeField] float speed = 3;
+
+    [Header("Oscillation - Amplitude & Direction")]
+    public Vector3 oscillationVector;
+    private Vector3 oldPos;
+    public float deltaPhase =  Mathf.PI;
+
+    void Start()
+    {
+        oldPos = transform.localScale;
+        oscillationVector = oldPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     
+
+        uint i = 0;
+        float phase = speed * Time.time + i * deltaPhase ;
+        transform.localScale = oldPos + oscillationVector * (Mathf.Sin(phase) + 4) ;
+        i++;
+
+    }
+}
