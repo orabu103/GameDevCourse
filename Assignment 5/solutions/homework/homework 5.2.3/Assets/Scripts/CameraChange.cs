50cd23c86afbe6417f80c57a1b4393fb7441111e
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-04-28 14:39:12 +0300

    Add camera change

---
+++ Assignment 5/solutions/homework/homework 5.2.3/Assets/Scripts/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+    [SerializeField] Camera camera1;
+    [SerializeField] Camera camera2;
+    bool b = false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+   
+    void Update()
+    {
+            if (Input.GetKey(KeyCode.M))
+             {
+                if (!b) { 
+                     camera1.gameObject.active = false;
+                    camera2.gameObject.active = true;
+                    b = true;
+                }
+                else if (b) { 
+                    camera1.gameObject.active = true;
+                    camera2.gameObject.active = false;
+                    b = false;
+                }
+            }
+    }
+}
---
+++ Assignment 5/solutions/homework/homework 5.2.3/Assets/Scripts/CameraChange2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange2 : MonoBehaviour
+{
+
+    [SerializeField] Camera camera1;
+    [SerializeField] Camera camera2;
+    bool b = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (!b)
+            {
+                camera1.gameObject.active = false;
+                camera2.gameObject.active = true;
+                b = true;
+            }
+            else if (b)
+            {
+                camera1.gameObject.active = true;
+                camera2.gameObject.active = false;
+                b = false;
+            }
+        }
+    }
+}
