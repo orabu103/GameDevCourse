5207f5ef724d07f5134e2118ee503cc0057fc231
unknown <danivngopro@gmail.com>
2020-04-28 10:48:16 +0300

    solutions to assignment 5 ex2.3 ex2.4

---
+++ Assignment 5/solutions/homework/ex5.2.4/Assets/Scripts/switchCams.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCams : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] Camera camera1;
+    [SerializeField] Camera camera2;
+    bool b = false;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !b)
+        {
+            camera1.gameObject.active = false;
+            camera2.gameObject.active = true;
+            b = true;
+        }
+        else if (Input.GetMouseButtonDown(0) && b)
+        {
+            camera1.gameObject.active = true;
+            camera2.gameObject.active = false;
+            b = false;
+        }
+    }
+}
