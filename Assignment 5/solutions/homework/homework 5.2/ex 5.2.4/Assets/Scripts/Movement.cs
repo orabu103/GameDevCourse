5eb2284e0fe6456ebbb11bbd7a82a9157d70b1a0
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-04-28 15:37:31 +0300

    homework 5 :100:

---
+++ Assignment 5/solutions/homework/homework 5.2/ex 5.2.4/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime * -1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime * -1, 0, 0);
+        }
+    }
+}
