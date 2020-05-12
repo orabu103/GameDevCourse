149d14087365bf17f95563408d7799bb5a0d0dce
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2020-05-12 11:09:45 +0300

    Final version :100:

---
+++ Assignment 7/solutions/homework/Assets/Script/DbigEnemy.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DbigEnemy : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    private int bigcCowLife = 10;
+    private int direction;
+    [SerializeField]
+    private float _speed = 1.25f;
+    [SerializeField]
+    private float _rotateSpeed = 0.3f;
+    [SerializeField]
+    private Player _player = null;
+    private bool _sensor = false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("police");
+        try
+        {
+            _player = GameObject.Find("Player").GetComponent<Player>();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("player not found");
+        }
+        direction = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position += new Vector3(0, -1 * _speed * Time.unscaledDeltaTime, 0);
+        if (direction == 1)
+            transform.Rotate(Vector3.forward * _rotateSpeed);
+        else
+            transform.Rotate(Vector3.back * 0.3f);
+        if (transform.position.y < -7f)
+        {
+            float randomized = Random.Range(-8.0f, 8.0f);
+            transform.position = new Vector3(randomized, 9, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+
+            bigcCowLife--;
+            Destroy(other.gameObject);
+            if(bigcCowLife < 1)
+            {
+                Destroy(this.gameObject);
+                FindObjectOfType<AudioManager>().Stop("police");
+                if (_player != null)
+                    _player.AddScore();
+            }
+        }
+        if (other.tag == "Player" && _sensor)
+        {
+            Destroy(this.gameObject);
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                FindObjectOfType<AudioManager>().Stop("police");
+                player.Damage();
+                player.Damage();
+                player.AddScore();
+                player.AddScore();
+
+            }
+        }
+    }
+    public void SetSensor()
+    {
+        _sensor = true;
+    }
+
+}
---
+++ Assignment 7/solutions/homework/Assets/Script/SpawnManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    public float time = 4f;
+    [SerializeField]
+    private GameObject _enemyPrefabs;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _powerUpPrefabs;
+    [SerializeField]
+    private float positive = 8f;
+    [SerializeField]
+    private float negative = -8f;
+    private bool Bigcow = false;
+    private int _countbigcow;
+    [SerializeField] GameObject bigCowObject;
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        while (true)
+        {
+            Vector3 postospawn = new Vector3(Random.Range(negative, positive), 13, 0);
+            GameObject new_powerUp = Instantiate(_powerUpPrefabs, postospawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(10, 15));
+        }
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (true)
+        {
+            if (!Bigcow)
+            {
+                Vector3 postospawn = new Vector3(Random.Range(negative, positive), 13, 0);
+                GameObject new_enemy = Instantiate(_enemyPrefabs, postospawn, Quaternion.identity);
+                new_enemy.transform.parent = _enemyContainer.transform;
+                if (Time.time % 10 == 0)
+                {
+                    time /= 2;
+                }
+                yield return new WaitForSeconds(time);
+            }
+            else
+            {
+                while (_countbigcow > 0)
+                {
+                    Debug.Log("big");
+                    Vector3 postospawn = new Vector3(Random.Range(negative, positive), 13, 0);
+                    GameObject new_enemy = Instantiate(bigCowObject, postospawn, Quaternion.identity);
+                    new_enemy.transform.parent = _enemyContainer.transform;
+                    if (Time.time % 10 == 0)
+                    {
+                        time /= 2;
+                    }
+                    Bigcow = false;
+                    yield return new WaitForSeconds(time);
+                    _countbigcow--;
+                }
+                Bigcow = false;
+            }
+        }
+    }
+    public void setBigCaw(int cow)
+    {
+        FindObjectOfType<AudioManager>().Play("police");
+             _countbigcow = cow;
+            Bigcow = true;
+    }
+}
